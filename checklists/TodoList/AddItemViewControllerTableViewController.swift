0142d5f50418ecefba3c389fc02af461a2dd0af8
Eduardo Moroni <eduardomoroni@gmail.com>
2018-03-28 16:46:45 -0300

    chapter-11: finishing chapter

--- checklists/TodoList/AddItemViewControllerTableViewController.swift
+++ checklists/TodoList/AddItemViewControllerTableViewController.swift
@@ -1,7 +1,8 @@
 import UIKit
 
-class AddItemViewControllerTableViewController: UITableViewController {
+class AddItemViewControllerTableViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,4 +28,19 @@ class AddItemViewControllerTableViewController: UITableViewController {
         print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated:true)
     }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange,with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
 }
+
