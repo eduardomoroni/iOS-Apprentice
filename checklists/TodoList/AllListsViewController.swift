2778537fb1c217c61a8d35a969faa6bad12d8982
Eduardo Moroni <eduardomoroni@gmail.com>
2018-03-29 15:55:19 -0300

    chapter-16: Add all list scene

---
+++ checklists/TodoList/AllListsViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class AllListsViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowChecklist", sender: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = makeCell(for: tableView)
+        cell.textLabel!.text = "List \(indexPath.row)"
+        return cell
+    }
+    
+    func makeCell(for tableView: UITableView) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
+            return cell
+        } else {
+            return UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+    }
+}
