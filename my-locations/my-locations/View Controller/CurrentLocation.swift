e33abe09863757e0dcdf2601440e9738ce9f2128
Eduardo Moroni <eduardomoroni@gmail.com>
2018-04-02 15:44:37 -0300

    chapter-21: style tag my location view

---
+++ my-locations/my-locations/View Controller/CurrentLocation.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func getLocation() {
+    }
+}
+
