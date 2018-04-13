b4180f3502ba728e8a8c6cc3f672e8658829550d
Eduardo Moroni <eduardomoroni@gmail.com>
2018-04-13 10:07:48 -0300

    feat: initial setup for shuffle songs

---
+++ shuffle-songs/shuffle-songs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shuffle-songs
+//
+//  Created by Eduardo Moroni on 13/04/18.
+//  Copyright © 2018 Eduardo Moroni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
